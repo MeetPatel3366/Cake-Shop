e6a770fa5aa901b5525726b86206ca80f7e1bdfd
Meet Patel <meet3366patel@gmail.com>
2024-11-10 20:48:50 -0800

    function to increment decrement card quantity

--- homeProductCards.js
+++ homeProductCards.js
@@ -1,3 +1,5 @@
+import { homeQuantityToggle } from "./homeQuantityToggle";
+
 const productContainer=document.querySelector("#productContainer");
 const productTemplate=document.querySelector("#productTemplate");
 
@@ -15,16 +17,22 @@ export const showProductContainer=(products)=>{
 
         const productClone=document.importNode(productTemplate.content,true);
 
+        productClone.querySelector("#cardValue").setAttribute("id",`card${id}`)
+
         productClone.querySelector(".productName").innerText=name
         productClone.querySelector(".category").innerText=category
         productClone.querySelector(".productImage").src=image
-        productClone.querySelector(".productImage").alt=name
+        productClone.querySelector(".productImage").alt=flavours
         productClone.querySelector(".productRating").innerText=`Rating: ${rating}`
         productClone.querySelector(".isVegetarian").innerText=`isVegetarian: ${isVegetarian}`
         productClone.querySelector(".productDescription").innerText=description
         productClone.querySelector(".productPrice").innerText=`₹${price}`
         productClone.querySelector(".productActualPrice").innerText=`₹${price*4}`
         productClone.querySelector(".productStock").innerText=stock
+
+        productClone.querySelector(".stockElement").addEventListener("click",(event)=>{
+            homeQuantityToggle(event,id,stock)
+        })
 
         productContainer.append(productClone)
     })
---
+++ homeQuantityToggle.js
@@ -0,0 +1,37 @@
+export const homeQuantityToggle=(event,id,stock)=>{
+    const currentCardElement=document.querySelector(`#card${id}`);
+    console.log(currentCardElement);
+
+    const productQuantity=currentCardElement.querySelector(".productQuantity");
+    console.log(productQuantity.innerText);
+
+    let quantity=parseInt(productQuantity.getAttribute("data-quantity")) || 1;
+
+    // console.log(event);
+    // console.log(event.target);
+    if(event.target.className=="cartIncrement")
+    {
+        if(quantity<stock)
+        {
+            quantity+=1;
+        }
+        else if(quantity==stock)
+        {
+            quantity=stock
+        }
+    }
+
+    if(event.target.className=="cartDecrement")
+    {
+        if(quantity>1)
+        {
+            quantity-=1;
+        }
+    }
+
+    productQuantity.innerText=quantity;
+
+    productQuantity.setAttribute("data-quantity",quantity.toString());
+
+    return quantity;
+}
